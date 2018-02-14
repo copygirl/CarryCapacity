e8f8fceb8f2457567fe1094ce4617d133dae9737
copygirl <copygirl@mcft.net>
2018-02-14 12:57:35 +0100

    Update directory structure

--- src/CarryCapacityMod.cs
+++ src/CarryCapacityMod.cs
@@ -13,7 +13,6 @@ namespace CarryCapacity
 			Description = "Adds the capability to carry various things",
 			Website     = "https://github.com/copygirl/CarryCapacity",
 			Author      = "copygirl",
-			Version     = "0.1.0",
 		};
 		
 		public static string MOD_ID => MOD_INFO.Name.ToLowerInvariant();
