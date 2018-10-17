7bf73021bcd6c0e53cc78ae94407e7c4872cba14
copygirl <copygirl@mcft.net>
2018-10-17 11:13:00 +0200

    Render held block in first person

--- src/Client/EntityCarryRenderer.cs
+++ src/Client/EntityCarryRenderer.cs
@@ -58,6 +58,8 @@ namespace CarryCapacity.Client
 		public double RenderOrder => 1.0;
 		public int RenderRange => 99;
 		
+		private float moveWobble;
+		
 		public void OnRenderFrame(float deltaTime, EnumRenderStage stage)
 		{
 			foreach (var player in API.World.AllPlayers) {
@@ -72,16 +74,14 @@ namespace CarryCapacity.Client
 				if (API.World == null) throw new Exception("API.World is null!");
 				if (API.World.Player == null) throw new Exception("API.World.Player is null!");
 				
-				// Don't render anything on the client player if they're in first person.
-				if ((API.World.Player.CameraMode == EnumCameraMode.FirstPerson)
-					&& (player == API.World.Player)) continue;
-				
 				var entity = player.Entity;
 				var allCarried = entity.GetCarried().ToList();
 				if (allCarried.Count == 0) continue; // Entity is not carrying anything.
 				
-				var renderApi    = API.Render;
-				var isShadowPass = (stage != EnumRenderStage.Opaque);
+				var renderApi     = API.Render;
+				var isShadowPass  = (stage != EnumRenderStage.Opaque);
+				var isFirstPerson = (player == API.World.Player)
+				                 && (API.World.Player.CameraMode == EnumCameraMode.FirstPerson);
 				
 				var renderer = (EntityShapeRenderer)entity.Properties.Client.Renderer;
 				if (renderer == null) continue; // Apparently this can end up being null?
@@ -91,32 +91,48 @@ namespace CarryCapacity.Client
 				if (animator == null) throw new Exception("renderer.curAnimator is null!");
 				
 				foreach (var carried in allCarried) {
+					var inHands = (carried.Slot == CarrySlot.Hands);
+					if (!inHands && isFirstPerson && !isShadowPass) continue;
+					
 					var renderSettings = _renderSettings[carried.Slot];
 					var renderInfo     = GetRenderInfo(carried);
 					
-					var viewMat  = Array.ConvertAll(API.Render.CameraMatrixOrigin, i => (float)i);
-					var modelMat = Mat4f.CloneIt(renderer.ModelMat);
+					var viewMat = Array.ConvertAll(API.Render.CameraMatrixOrigin, i => (float)i);
+					float[] modelMat;
 					
-					if (!animator.AttachmentPointByCode.TryGetValue(renderSettings.AttachmentPoint, out var pose)) continue;
-					var animModelMat = pose.Pose.AnimModelMatrix;
-					Mat4f.Mul(modelMat, modelMat, animModelMat);
-					
-					IStandardShaderProgram prog = null;
-					
-					if (isShadowPass) {
-						renderApi.CurrentActiveShader.BindTexture2D("tex2d", renderInfo.TextureId, 0);
+					if (inHands && isFirstPerson && !isShadowPass) {
+						modelMat = Mat4f.Invert(Mat4f.Create(), viewMat);
+						
+						if (entity.Controls.TriesToMove) {
+							var moveSpeed = entity.Controls.MovespeedMultiplier * (float)entity.GetWalkSpeedMultiplier();
+							moveWobble += moveSpeed * deltaTime * 5.0F;
+						} else {
+							var target = (float)(Math.Round(moveWobble / Math.PI) * Math.PI);
+							var speed = deltaTime * (0.2F + Math.Abs(target - moveWobble) * 4);
+							if (Math.Abs(target - moveWobble) < speed) moveWobble = target;
+							else moveWobble += Math.Sign(target - moveWobble) * speed;
+						}
+						moveWobble = moveWobble % (GameMath.PI * 2);
+						
+						var moveWobbleOffsetX = GameMath.Sin((moveWobble + GameMath.PI)) * 0.03F;
+						var moveWobbleOffsetY = GameMath.Sin(moveWobble * 2) * 0.02F;
+						
+						Mat4f.Translate(modelMat, modelMat, moveWobbleOffsetX, moveWobbleOffsetY - 0.25F, -0.25F);
+						Mat4f.RotateY(modelMat, modelMat, 90.0F * GameMath.DEG2RAD);
 					} else {
-						prog = renderApi.PreparedStandardShader((int)entity.Pos.X, (int)entity.Pos.Y, (int)entity.Pos.Z);
-						prog.Tex2D = renderInfo.TextureId;
-						prog.AlphaTest = 0.01f;
+						modelMat = Mat4f.CloneIt(renderer.ModelMat);
+						
+						if (!animator.AttachmentPointByCode.TryGetValue(renderSettings.AttachmentPoint, out var attachPointAndPose)) continue;
+						var animModelMat = attachPointAndPose.Pose.AnimModelMatrix;
+						Mat4f.Mul(modelMat, modelMat, animModelMat);
+						
+						// Apply attachment point transform.
+						var attach = attachPointAndPose.AttachPoint;
+						Mat4f.Translate(modelMat, modelMat, (float)(attach.PosX / 16), (float)(attach.PosY / 16), (float)(attach.PosZ / 16));
+						Mat4f.RotateX(modelMat, modelMat, (float)attach.RotationX * GameMath.DEG2RAD);
+						Mat4f.RotateY(modelMat, modelMat, (float)attach.RotationY * GameMath.DEG2RAD);
+						Mat4f.RotateZ(modelMat, modelMat, (float)attach.RotationZ * GameMath.DEG2RAD);
 					}
-					
-					// Apply attachment point transform.
-					var attach = pose.AttachPoint;
-					Mat4f.Translate(modelMat, modelMat, (float)(attach.PosX / 16), (float)(attach.PosY / 16), (float)(attach.PosZ / 16));
-					Mat4f.RotateX(modelMat, modelMat, (float)attach.RotationX * GameMath.DEG2RAD);
-					Mat4f.RotateY(modelMat, modelMat, (float)attach.RotationY * GameMath.DEG2RAD);
-					Mat4f.RotateZ(modelMat, modelMat, (float)attach.RotationZ * GameMath.DEG2RAD);
 					
 					// Apply carried block's behavior transform.
 					var t = renderInfo.Transform;
@@ -130,17 +146,24 @@ namespace CarryCapacity.Client
 					Mat4f.Translate(modelMat, modelMat, t.Translation.X, t.Translation.Y, t.Translation.Z);
 					
 					if (isShadowPass) {
+						var prog = renderApi.CurrentActiveShader;
 						Mat4f.Mul(modelMat, API.Render.CurrentShadowProjectionMatrix, modelMat);
-						API.Render.CurrentActiveShader.UniformMatrix("mvpMatrix", modelMat);
-						API.Render.CurrentActiveShader.Uniform("origin", renderer.OriginPos);
+						prog.BindTexture2D("tex2d", renderInfo.TextureId, 0);
+						prog.UniformMatrix("mvpMatrix", modelMat);
+						prog.Uniform("origin", renderer.OriginPos);
+						
+						API.Render.RenderMesh(renderInfo.ModelRef);
 					} else {
+						var prog = renderApi.PreparedStandardShader((int)entity.Pos.X, (int)entity.Pos.Y, (int)entity.Pos.Z);
+						prog.Tex2D       = renderInfo.TextureId;
+						prog.AlphaTest   = 0.01f;
 						prog.ViewMatrix  = viewMat;
 						prog.ModelMatrix = modelMat;
+						
+						API.Render.RenderMesh(renderInfo.ModelRef);
+						
+						prog.Stop();
 					}
-					
-					API.Render.RenderMesh(renderInfo.ModelRef);
-					
-					prog?.Stop();
 				}
 			}
 		}
