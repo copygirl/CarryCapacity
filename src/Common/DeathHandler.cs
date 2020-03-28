6952dcc7ad57d6237fcf3f31dd6f0cd90d9f7fd0
copygirl <copygirl@mcft.net>
2020-03-28 23:56:29 +0100

    Don't drop blocks when deathPunishment=keep

--- src/Common/DeathHandler.cs
+++ src/Common/DeathHandler.cs
@@ -8,7 +8,17 @@ namespace CarryCapacity.Common
 		public DeathHandler(ICoreServerAPI api)
 			=> api.Event.PlayerDeath += OnPlayerDeath;
 		
+		/// <summary>
+		///   Only drop carried blocks if "deathPunishment" isn't set to "keep".
+		///   This is how the vanilla game checks whether it should drop inventory contents.
+		///   
+		///   NOTE: Taking damage will still drop blocks not carried on back.
+		///   See <see cref="Server.EntityBehaviorDropCarriedOnDamage"/> for that.
+		/// </summary>
 		private void OnPlayerDeath(IPlayer player, DamageSource source)
-			=> player.Entity.DropAllCarried();
+		{
+			if (player.Entity.Properties.Server?.Attributes?.GetBool("keepContents", false) != true)
+				player.Entity.DropAllCarried();
+		}
 	}
 }
